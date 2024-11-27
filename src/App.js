320b90d7223040bf8f8559d5a85523e2c3e75cd0
jamarbri <168193675+jamarbri@users.noreply.github.com>
2024-11-27 14:12:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './dragon-ball.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Ejercicio de publicación
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
